de882d16da0d4b3712cb6c3912f5889b8601af58
Carlos Ferraz <carloslf@e.email>
2022-10-12 22:45:00 +0300

    Added fetch to test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const contentful = require('contentful');
+
+const client = contentful.createClient({
+  space: 'fx3pnargdzjr',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: 'XK9hAS_c-tQHK5UcRKKF-wUZhCUz8nE9ExQ5-9MD5qA',
+});
+
+const App = () => {
+  const [testEntries, setTestEntries] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      await client.getEntries()
+        .then((response) => setTestEntries(response.items)).then(console.log(testEntries))
+        .catch(console.error);
+    };
+    fetchData();
+  }, []);
+  
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-      Hello Gentlemen! Shall we cook?
+        Hello Gentlemen! Shall we cook?
       </h1>
     </div>
   );
-}
+};
 
 export default App;
+
